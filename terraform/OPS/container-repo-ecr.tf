27548bb830b0c06392725de2731819bb99a25859
Tom Broughton <hello@tombroughton.me>
2022-11-03 00:37:57 +0000

    added ecr policies

--- terraform/OPS/container-repo-ecr.tf
+++ terraform/OPS/container-repo-ecr.tf
@@ -1,4 +1,29 @@
-resource "aws_ecr_repository" "container_repo_ecr" {
+resource "aws_ecr_repository" "container_deployment_ecr" {
   name                 = var.ECR
   image_tag_mutability = "MUTABLE"
+}
+
+
+resource "aws_ecr_repository_policy" "container_deployment_ecr_policy" {
+  repository = aws_ecr_repository.contianer_deployment_ecr
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "allow-env-deploy-from-ecr",
+        Effect = "Allow",
+        Principal = {
+          AWS = [ var.var.environments_gha_oidc_role_arns]
+        },
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages",
+          "ecr:DescribeRepositories",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:ListImages"
+        ]
+      }
+    ]  
+  })
 }
--- terraform/OPS/variables.tf
+++ terraform/OPS/variables.tf
@@ -1,6 +1,7 @@
+#Whenever a new env is created with a new oidc role, the role needs to be added here and ops tf needs to be applied
 variable "environments_gha_oidc_role_arns" {
   type        = list(string)
-  description = "The environments managed by this account"
+  description = "The environments that need to access the deployment resources"
   default = [
     "arn:aws:iam::123213212312:role/github-actions-oidc-role",
     "arn:aws:iam::234232342323:role/github-actions-oidc-role",
